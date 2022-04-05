19cdfc702a8223b829957846b0d176b88ebdf0d5
asd <jo7334an-s@student.lu.se>
2022-04-05 14:01:39 +0200

    Fixed button

--- app/src/main/java/com/example/medtechapp/MainActivity.java
+++ app/src/main/java/com/example/medtechapp/MainActivity.java
@@ -1,50 +1,19 @@
 package com.example.medtechapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.medtechapp.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-    private Button moveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-
-        moveButton = (Button) findViewById(R.id.moveButton);
-        moveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMoveActivity();
-            }
-        });
-
+        setContentView(R.layout.activity_main);
     }
 
-    public void openMoveActivity(){
+    public void openMoveActivity(View view){
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
